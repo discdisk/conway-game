5715bd908756275cb869ab188544e0ee26cc9dc2
discdisk <32610374+discdisk@users.noreply.github.com>
2020-01-13 16:19:18 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Game of Life
+// Video: https://youtu.be/FWSR_7kZuYg
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+let speed ;
+
+function setup() {
+  slider = createSlider(0, 5000, 1000);
+  slider.position(10, 10);
+  text = createP('speed:'+slider.value())
+  text.position(100, 0);
+  slider.style('width', '80px');
+
+
+  cnv=createCanvas(600, 400);
+  cnv.position(5,40);
+  cols = width / resolution;
+  rows = height / resolution;
+
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (random(10)>7){
+        grid[i][j] = 0
+      }
+      else{
+        grid[i][j] = 1
+      }
+        
+    }
+  }
+
+  drawpic();
+}
+function draw(){
+    speed=slider.value();
+    text.html('speed:'+speed);
+}
+
+
+
+function drawpic() {
+  background(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+  let next = make2DArray(cols, rows);
+
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      // let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+
+  grid = next;
+  
+  setTimeout(drawpic,speed);
+}
+
+
+
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
